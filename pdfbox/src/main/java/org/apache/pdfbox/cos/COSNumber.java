5c5511298c33bbbe9ccb1f1c763953de9452c103
faltiska <alfred.faltiska@outlook.com>
2020-07-14 10:05:51 +0300

    Updated from upstream trunk

--- pdfbox/src/main/java/org/apache/pdfbox/cos/COSNumber.java
+++ pdfbox/src/main/java/org/apache/pdfbox/cos/COSNumber.java
@@ -57,19 +57,19 @@ public abstract class COSNumber extends COSBase
      */
     public static COSNumber get( String number ) throws IOException
     {
-        if (number.length() == 1)
+        if (number.length() == 1) 
         {
             char digit = number.charAt(0);
-            if ('0' <= digit && digit <= '9')
+            if ('0' <= digit && digit <= '9') 
             {
                 return COSInteger.get(digit - '0');
-            }
-            else if (digit == '-' || digit == '.')
+            } 
+            else if (digit == '-' || digit == '.') 
             {
                 // See https://issues.apache.org/jira/browse/PDFBOX-592
                 return COSInteger.ZERO;
-            }
-            else
+            } 
+            else 
             {
                 throw new IOException("Not a number: " + number);
             }
